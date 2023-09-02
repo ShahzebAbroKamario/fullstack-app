7a057053a1ee62712748684fc32c3260868e6c81
ShahzebAbroKamario <shahzebaliabro12345@gmail.com>
2023-09-01 23:18:14 -0700

    Modified backend/server.js

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import cookieParser from "cookie-parser";
+import path from "path";
 
 const port = process.env.PORT || 5000;
 
